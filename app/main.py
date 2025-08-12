15d1d19859570a679e73ea7deac396c3188e5922
Vedang Barhate <vedang.inbox@gmail.com>
2025-08-12 16:44:54 -0700

    feat: added railway config with uvicorn (#1)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    # TODO: remove all the prints with the logger
+    print("Starting FLUXX-API application")
+    yield
+    print("Shutting down FLUXX-API application")
+
+app = FastAPI(
+    title="My FastAPI App",
+    version="0.1.0",
+    lifespan=lifespan
+)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Bigger Applications!"}
+    """Root route of the API"""
+    return {"message": "Hello billing"}
